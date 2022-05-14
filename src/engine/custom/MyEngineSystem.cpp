f48ca977c99e0f1fce53c6a781489175f7c13dc3
shashs20 <69312805+shashs20@users.noreply.github.com>
2022-05-14 01:32:44 +0100

    Update MyEngineSystem.cpp

--- src/engine/custom/MyEngineSystem.cpp
+++ src/engine/custom/MyEngineSystem.cpp
@@ -1,1 +1,15 @@
-#include "MyEngineSystem.h"
+#ifndef __MY_ENGINE_H__
+#define __MY_ENGINE_H__
+
+#include <SDL_mixer.h>
+
+#include "../EngineCommon.h"
+
+class MyEngineSystem {
+	friend class XCube2Engine;
+	private:
+		int sound(int argc, char** argv);
+
+	public:
+};
+#endif
