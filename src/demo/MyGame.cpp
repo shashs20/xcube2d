0f59e6cb0db23f533ed3e4e5d18aab2fd01015ef
shashs20 <69312805+shashs20@users.noreply.github.com>
2022-08-18 01:22:47 +0100

    Update MyGame.cpp

--- src/demo/MyGame.cpp
+++ src/demo/MyGame.cpp
@@ -1,6 +1,6 @@
 #include "MyGame.h"
 
-MyGame::MyGame() : AbstractGame(), score(0), lives(3), numKeys(5), gameWon(false), box(5, 5, 30, 30) {
+MyGame::MyGame() : AbstractGame(), score(0), lives(3), numKeys(5), enemyObjects(10), gameWon(false), gameLose(false),  box(5, 5, 30, 30) {
 	TTF_Font * font = ResourceManager::loadFont("res/fonts/arial.ttf", 72);
 	gfx->useFont(font);
 	gfx->setVerticalSync(true);
@@ -11,6 +11,13 @@ MyGame::MyGame() : AbstractGame(), score(0), lives(3), numKeys(5), gameWon(false
         k->pos = Point2(getRandom(0, 750), getRandom(0, 550));
         gameKeys.push_back(k);
     }
+
+	for (int i = 0; i < enemyObjects; i++) {
+		std::shared_ptr<Enemy> k = std::make_shared<Enemy>();
+		k->isAlive = true;
+		k->pos = Point2(getRandom(0, 750), getRandom(0, 550));
+		enemy.push_back(k);
+	}
 }
 
 MyGame::~MyGame() {
@@ -50,21 +57,38 @@ void MyGame::update() {
 	}
 
 	velocity.x = 0;
-    velocity.y = 0;
+	velocity.y = 0;
 
 	if (numKeys == 0) {
 		gameWon = true;
 	}
+																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																									
+	for (auto key : enemy) {
+		if (key->isAlive && box.contains(key->pos)) {
+			score -= 100;
+			key->isAlive = false;
+			enemyObjects--;
+		}
+	}
+
+	if (enemyObjects == 9) {
+		gameLose = true;
+	}
 }
 
 void MyGame::render() {
-	gfx->setDrawColor(SDL_COLOR_RED);
+	gfx->setDrawColor(SDL_COLOR_GREEN);
 	gfx->drawRect(box);
+
+	gfx->setDrawColor(SDL_COLOR_RED);
+	for (auto key : enemy)
+		if (key->isAlive)
+			gfx->drawCircle(key->pos, 5);
 
 	gfx->setDrawColor(SDL_COLOR_YELLOW);
 	for (auto key : gameKeys)
-        if (key->isAlive)
-		    gfx->drawCircle(key->pos, 5);
+		if (key->isAlive)
+			gfx->drawCircle(key->pos, 5);
 }
 
 void MyGame::renderUI() {
@@ -73,5 +97,8 @@ void MyGame::renderUI() {
 	gfx->drawText(scoreStr, 780 - scoreStr.length() * 50, 25);
 
 	if (gameWon)
-		gfx->drawText("YOU WON", 250, 500);
+		gfx->drawText("YOU WON", 200, 500);
+
+	if (gameLose)
+		gfx->drawText("GAME OVER", 190, 500);
 }
